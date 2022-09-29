476a57af06e7fb510d60b1440fc343813ff3bb0c
Alexey Peremyshlennikov <adambrn@mail.ru>
2022-09-29 19:52:26 +0700

    solutions on c#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+            Console.Write($"{array[i]} \t");          
+    }
+}
+string[] newArray;
+Console.Write("Введите элементы массива через запятую: ");
+string? stringArray = Console.ReadLine();
+string[] array = stringArray.Split(',').ToArray();
+
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+
+Console.WriteLine();
+
+Console.Write("Введите максимальную длинну элемента: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Полученный массив:");
+newArray = (from elem in array where elem.Length <= n select elem).ToArray();
+
+PrintArray(newArray);
